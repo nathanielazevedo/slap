b1fb1a8ae3e1db6ce648f5461b0e7edea76f91d2
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-11-05 15:36:34 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const db = require("./db");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+/** Add a new instructor. */
+
+/** Show JSON on instructor */
+
+app.get("/addslap", async function (req, res, next) {
+  const results = await db.query(`SELECT slap FROM shouldIslaptable`);
+  let number = results.rows[0].slap; 
+  number++ ;
+  await db.query(`UPDATE shouldIslaptable SET slap=${number} WHERE id = 1`); 
+  return res.json({message: 'updated successfully'});
+});
+
+
+app.get("/slap", async function (req, res, next) {
+  const results = await db.query(`SELECT slap FROM shouldIslaptable`);
+  let number = results.rows[0].slap; 
+  if(number !== 0){
+    number--;
+    await db.query(`UPDATE shouldIslaptable SET slap=${number} WHERE id = 1`); 
+  }
+  return res.json(results.rows[0].slap);
+});
+
+app.get('/', function(req, res) {
+  return res.send('hey');
+});
+
+app.listen(3000, function () {
+  console.log('App on port 3000');
+})
